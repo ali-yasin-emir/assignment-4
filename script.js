d0a44b8fc2a6cecb2a13be8844cf0208617790ab
Ali Yasin Emir <89742020+ali-yasin-emir@users.noreply.github.com>
2022-10-21 10:34:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// 1
+let my_country = 80;
+my_country /= 2;
+
+// 2
+my_country++;
+console.log(my_country);
+
+// 3
+let finland = 6;
+console.log(my_country > finland);
+
+// 4
+let average_population = 33;
+console.log(my_country < average_population);
+
+//5
+country = "Portugal";
+continent = "Europe";
+population = 11;
+language = "Portugese";
+
+description =
+  country +
+  " " +
+  "is in" +
+  " " +
+  continent +
+  ", and its" +
+  " " +
+  11 +
+  " " +
+  "million speak" +
+  " " +
+  language;
+
+console.log(description);
